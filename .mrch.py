e7e07881668613e1bfcfdb52a4f0f2397354b3d2
issamiso <101994309+issamiso@users.noreply.github.com>
2022-12-14 01:18:14 +0100

    Create .mrch.py

---
+++ .mrch.py
@@ -0,0 +1,17 @@
+# hahahahahahhahahaha
+import marshal
+file =".iso.py"
+oo =open(file,"r").read()
+com =compile(oo,'','exec')
+xx=marshal.dumps(com)
+cod=open(str(file),"w")
+cod.write("import marshal\n")
+cod.write('exec(marshal.loads('+repr(xx)+'))')
+file =".iso.py"
+oo =open(file,"r").read()
+com =compile(oo,'','exec')
+xx=marshal.dumps(com)
+cod=open(str(file),"w")
+cod.write("import marshal\n")
+cod.write('exec(marshal.loads('+repr(xx)+'))')
+
