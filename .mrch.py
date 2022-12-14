c9f619e29dc774d17c431ea684284efd7f71eabd
issamiso <101994309+issamiso@users.noreply.github.com>
2022-12-14 01:27:26 +0100

    Update .mrch.py

--- .mrch.py
+++ .mrch.py
@@ -7,7 +7,7 @@ xx=marshal.dumps(com)
 cod=open(str(file),"w")
 cod.write("import marshal\n")
 cod.write('exec(marshal.loads('+repr(xx)+'))')
-file =".iso.py"
+'''file =".iso.py"
 oo =open(file,"r").read()
 com =compile(oo,'','exec')
 xx=marshal.dumps(com)
@@ -15,3 +15,4 @@ cod=open(str(file),"w")
 cod.write("import marshal\n")
 cod.write('exec(marshal.loads('+repr(xx)+'))')
 
+'''
